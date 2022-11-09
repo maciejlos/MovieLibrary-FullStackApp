b091f84df4fe05fc853a1a485f764f68d46cf49f
maciejlos <117121086+maciejlos@users.noreply.github.com>
2022-11-09 16:34:41 +0100

    Add files via upload

---
+++ src/main/java/com/magicmoose/movielibrary/Movie.java
@@ -0,0 +1,15 @@
+package com.magicmoose.movielibrary;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Movie {
+    private int id;
+    private String name;
+    private int rating;
+}
